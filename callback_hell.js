92e4e09da2e98dfb5ef8ec80b243408a5a5885b6
Rory Dahl <rsdah13@gmail.com>
2017-10-27 12:59:11 -0700

    Callbacks replaced with promises to avoid Callback Hell.

---
+++ callback_hell.js
@@ -0,0 +1,35 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+const dboper = require('./callbackOperations'); //dboperations
+const url = 'mongodb://localhost:27017/conFusion';
+
+MongoClient.connect(url, (err, db) => {
+	assert.equal(err,null);
+
+	console.log('Connected correctly to server.');
+
+	dboper.insertDocument(db, {name: "VaDonut", description: "Test donut"},
+	 "dishes", (result)=>{
+		console.log("Inset Document:\n", result.ops);
+
+		dboper.findDocuments(db, "dishes", (docs) => {
+			console.log("Found Documents:\n", docs);
+
+			dboper.updateDocument(db, {name: "VaDonut"},
+		 		{description: "Updated Test Donut"}, "dishes", (result)=>{
+					console.log("Updated Document:\n", result.result);
+
+					dboper.findDocuments(db, "dishes", (docs) => {
+						console.log("Found Update Documents:\n", docs);
+
+						db.dropCollection("dishes", (result)=>{
+							console.log("Dropped collection: ", result);
+
+							db.close();
+						});
+					});
+				});
+		});
+	});
+});
--- index.js
+++ index.js
@@ -4,32 +4,38 @@ const assert = require('assert');
 const dboper = require('./operations'); //dboperations
 const url = 'mongodb://localhost:27017/conFusion';
 
-MongoClient.connect(url, (err, db) => {
-	assert.equal(err,null);
+MongoClient.connect(url).then((db) => {
 
 	console.log('Connected correctly to server.');
 
-	dboper.insertDocument(db, {name: "VaDonut", description: "Test donut"},
-	 "dishes", (result)=>{
-		console.log("Inset Document:\n", result.ops);
+	dboper.insertDocument(db, {name: "VaDonut", description: "Test donut"}, "dishes")
+	 .then((result)=>{
+			console.log("Inset Document:\n", result.ops);
 
-		dboper.findDocuments(db, "dishes", (docs) => {
+			dboper.findDocuments(db, "dishes");
+		})
+		.then((docs) => {
 			console.log("Found Documents:\n", docs);
 
-			dboper.updateDocument(db, {name: "VaDonut"},
-		 		{description: "Updated Test Donut"}, "dishes", (result)=>{
-					console.log("Updated Document:\n", result.result);
+			return dboper.updateDocument(db, {name: "VaDonut"},
+		 		{description: "Updated Test Donut"}, "dishes");
+		})
+		.then((result) => {
+			console.log("Updated Document:\n", result.result);
 
-					dboper.findDocuments(db, "dishes", (docs) => {
-						console.log("Found Update Documents:\n", docs);
+			return dboper.findDocuments(db, "dishes");
+		})
+		.then((docs) => {
+			console.log("Found Update Documents:\n", docs);
 
-						db.dropCollection("dishes", (result)=>{
-							console.log("Dropped collection: ", result);
+			return db.dropCollection("dishes");
+		})
+		.then((result) => {
+				console.log("Dropped collection: ", result);
 
-							db.close();
-						});
-					});
-				});
-		});
-	});
-});
+				return db.close();
+		})
+		.catch((err) => console.log(err));
+
+}, (err) => {console.log(err)})
+.catch((err) => console.log(err));
